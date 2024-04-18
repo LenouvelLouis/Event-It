d2fb334c18a6d2bc62598fe022289a23e093df82
LenouvelLouis <117534107+LenouvelLouis@users.noreply.github.com>
2024-04-18 22:58:12 +0200

    start try

--- MVC/index.php
+++ MVC/index.php
@@ -1,13 +1,21 @@
 <?php
 session_start();
-$controle = isset($_GET['controle'])?($_GET['controle']):NULL;
-$action = isset($_GET['action'])?($_GET['action']):NULL;
-if($controle == NULL || $action == NULL){
-    require(("./controle/home.php"));
-    ident();
-}
-else {
-    require ("./controle/" . $controle . ".php");
-    $action();
+$path = isset($_GET['path'])? explode('/', trim($_GET['path'], '/')) : [];
+$controle = isset($path[0])? $path[0] : NULL;
+$action = isset($path[1])? $path[1] : NULL;
+
+switch ($controle) {
+    case NULL:
+        require("./controle/home.php");
+        ident();
+        break;
+    case 'pages':
+        require("./controle/pages/" . $action . ".php");
+        $action();
+        break;
+    default:
+        require("./controle/" . $controle . ".php");
+        $action();
+        break;
 }
 ?>
